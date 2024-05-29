c3ee71f31b2a1200763b6826b1c5f03ee0adac96
Jkose7 <ojosema10@gmail.com>
2024-05-29 15:22:08 -0500

    setup project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ColorChangeDiv from './components/ColorChangeDiv.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ColorChangeDiv
   }
 }
+
 </script>
 
+<template>
+  <main class="color-change-section">
+    <ColorChangeDiv />
+    <ColorChangeDiv />
+    <ColorChangeDiv />
+  </main>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.color-change-section {
+  display: flex;
+  gap: 2rem;
+  padding: 2rem;
+  width: 100%;
+  height: 100vh;
 }
 </style>
---
+++ src/components/ColorChangeDiv.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  name: 'ColorChangeDiv',
+  data() {
+    return {
+      color: this.changeColor()
+    }
+  },
+  methods: {
+    changeColor() {
+      let r = Math.floor(Math.random() * 255)
+      let g = Math.floor(Math.random() * 255)
+      let b = Math.floor(Math.random() * 255)
+
+      const color = `rgb(${r}, ${g}, ${b})`
+      return color
+    },
+    getColor() {
+      this.color = this.changeColor() 
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="color-change-container" :style="{backgroundColor: color}" @click="getColor">
+  </div>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.color-change-container {
+  display: flex;
+
+  border-radius: 8px;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  background-color: aquamarine;
+  transition: all .3s;
+
+  &:hover {
+    transform: scale(1.01);
+  }
+}
+</style>
